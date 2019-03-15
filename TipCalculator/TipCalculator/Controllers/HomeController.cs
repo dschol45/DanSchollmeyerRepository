2fbeb739d0d8264e8d5225bb13faa93b5423fde2
RNPC <schollmeyer.dan@gmail.com>
2019-03-14 23:57:48 -0500

    Tip Calculator and Weather

---
+++ TipCalculator/TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Tip(Tip model)
+        {
+            return View("TipResult", model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Tip model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("TipResult", model);
+            }
+            else
+            {
+                return View("Index", model);
+            }
+        }
+    }
+}
---
+++ TipCalculator/TipCalculator/Models/Tip.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TipCalculator.Models
+{
+    public class Tip : IValidatableObject
+    {
+        public decimal Total { get; set; }
+        public decimal? TipPercent { get; set; }
+        public decimal TipAmount
+        {
+            get
+            {
+                if (decimal.TryParse(TipPercent.ToString(), out decimal TipPercentValue))
+                {
+                    return Total * TipPercentValue/100;
+                }
+                return 0;
+            }
+        }
+        public decimal NewTotal => Total + TipAmount;
+        public int Patrons { get; set; }
+        public decimal Split => NewTotal / Patrons;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (Total <= 0.01m)
+            {
+                errors.Add(new ValidationResult("Total must be at least $0.01", new[] { "Total" }));
+            }
+
+            if (TipPercent is decimal)
+            {
+                decimal percent = (decimal)TipPercent;
+                if (percent < 0)
+                {
+                    errors.Add(new ValidationResult("Tip Percent cannot be negative!", new[] { "TipPercent" }));
+                }
+            }
+            return errors;
+        }
+    }
+}
